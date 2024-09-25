fb418946183acfe57d33fb54050f48925a0bcdcb
Aarsh Patel <aarsh18patel@gmail.com>
2024-09-25 18:33:49 +0530

    now user can see multiple alerts by starting, pausing, reseting and completing countdown. start, pause and reset button works

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const counting = document.querySelector("#counting")
 const startBtn = $("#startBtn");
 const pauseBtn = $("#pauseBtn");
 const resetBtn = $("#resetBtn");
-const completeCountdown = document.querySelector("#alert")
+const showAlerts = document.querySelector("#alert")
 
 let countdown = parseInt(counting.innerText);
 var start, pause, reset
 
 startBtn.on("click", function () {
     clearInterval(start)
+    showAlerts.innerHTML = `
+    <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <div>Hey! Your countdown started...!</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `
     let customTime = document.querySelector("#countdown").value
     if (customTime) {
         console.log(customTime)
@@ -21,13 +27,35 @@ startBtn.on("click", function () {
         } else {
             // alert("Time Out")
             clearInterval(start)
-            completeCountdown.innerHTML = `
+            showAlerts.innerHTML = `
              <div class="alert alert-success alert-dismissible" role="alert">
-                <div>Hey! Your countdown is complete..!</div>
+                <div>Hey! Your countdown is complete...!</div>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        `
+            `
         }
     }, 1000);
 })
 
+pauseBtn.on("click", function () {
+    showAlerts.innerHTML += `
+    <div class="alert alert-warning alert-dismissible" role="alert">
+        <div>Hey! Click start to continue countdown...!</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `
+    clearInterval(start)
+})
+
+
+resetBtn.on("click", function () {
+    showAlerts.innerHTML = `
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <div>Hey! You've reset the countdown...!</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `
+    clearInterval(start)
+    counting.innerText = 10
+    countdown = 10
+})
