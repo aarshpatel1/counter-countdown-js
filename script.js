e48f9009a9ceacf8a30be6c31525533ed1ed059e
Aarsh Patel <aarsh18patel@gmail.com>
2024-09-25 18:01:53 +0530

    solve issue in starting countdown

--- script.js
+++ script.js
@@ -2,29 +2,32 @@ const counting = document.querySelector("#counting")
 const startBtn = $("#startBtn");
 const pauseBtn = $("#pauseBtn");
 const resetBtn = $("#resetBtn");
+const completeCountdown = document.querySelector("#alert")
 
 let countdown = parseInt(counting.innerText);
 var start, pause, reset
 
 startBtn.on("click", function () {
-
+    clearInterval(start)
     let customTime = document.querySelector("#countdown").value
     if (customTime) {
         console.log(customTime)
-        countdown = customTime + 1
+        countdown = customTime
     }
     start = setInterval(() => {
         if (countdown > 0) {
             countdown -= 1
-            updateCounter(countdown)
+            counting.innerText = countdown
         } else {
             // alert("Time Out")
             clearInterval(start)
-            updateCounter("Time Out")
+            completeCountdown.innerHTML = `
+             <div class="alert alert-success alert-dismissible" role="alert">
+                <div>Hey! Your countdown is complete..!</div>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `
         }
     }, 1000);
 })
 
-function updateCounter(currentCount) {
-    counting.innerText = currentCount
-}
