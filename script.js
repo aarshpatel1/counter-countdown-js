fbc28f31600963ef3019a4bcd54c4dc9b1d81a94
Aarsh Patel <aarsh18patel@gmail.com>
2024-09-25 23:47:41 +0530

    change font to roboto mono and ui fix

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ startBtn.on("click", function () {
 })
 
 pauseBtn.on("click", function () {
-    showAlerts.innerHTML += `
+    showAlerts.innerHTML = `
     <div class="alert alert-warning alert-dismissible" role="alert">
         <div>Hey! Click start to continue countdown...!</div>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
